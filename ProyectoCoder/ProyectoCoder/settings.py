265dcc781c2607a624abe21faee142890a5346a4
hanoirocker <103009788+hanoirocker@users.noreply.github.com>
2022-06-14 22:45:56 -0300

    bla

--- ProyectoCoder/ProyectoCoder/settings.py
+++ ProyectoCoder/ProyectoCoder/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os #a importar para los templates en STATICS_DIR
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,6 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Se agregan estas linea para trabajar con los templates descargados
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), #'static' es el nombre de la carpeta static justamente, acá le decimos que vaya a buscar esos templates ahí.
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
